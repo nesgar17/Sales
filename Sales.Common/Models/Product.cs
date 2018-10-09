ef8b0d5a80856f14ffcb2ec5727e746c76cb5787
nesgar17 <39680968+nesgar17@users.noreply.github.com>
2018-10-09 13:35:54 -0500

    conexion

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishOn { get; set; }
+    }
+}
